3587c46a8e06ca009b439f7791d88125dd05d5e5
CharlesThomaz <charlesthomaz87@gmail.com>
2023-12-03 16:16:08 -0300

    js

---
+++ JavaScript/js/js.js
@@ -0,0 +1,22 @@
+// comentarios js
+
+/* linha de comentario
+ linha de comentario
+ linha de comentario */ 
+
+//  Usando console
+console.log("Usando o console");
+console.log('Olá eu sou o console');
+console.log(`Olá eu sou o console`);
+
+
+let nome = 'charles';
+let email = 'charlesthomaz87@gmail.com';
+let idade = 36;
+let logado = true; 
+
+console.log(nome);
+console.log(email);
+console.log(idade);
+console.log(logado);
+
